491e41ed8d0f31dd63c58f65917798d39f16e03a
swheatley <shaylee1@me.com>
2018-02-09 12:09:16 -0700

    PHP MYSQL Essential TRAINING

---
+++ Ex_Files_PHP_MySQL_EssT_Basics/Exercise Files/Chapter_01/01_02/globe_bank/public/staff/index.php
@@ -0,0 +1,12 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php $page_title = 'Staff Menu'; ?>
+<?php include('../../private/shared/staff_header.php'); ?>
+
+
+<div id="content">
+
+</div>
+
+
+<?php include('../../private/shared/staff_footer.php'); ?>
