7ac452a7806098fab9de7295efb4cb8cd9b0b42a
swheatley <shaylee1@me.com>
2018-02-20 09:03:58 -0700

    Adding staff page to CMS

--- Ex_Files_PHP_MySQL_EssT_Basics/Exercise Files/Chapter_01/01_02/globe_bank/public/staff/index.php
+++ Ex_Files_PHP_MySQL_EssT_Basics/Exercise Files/Chapter_01/01_02/globe_bank/public/staff/index.php
@@ -1,12 +1,19 @@
 <?php require_once('../../private/initialize.php'); ?>
 
 <?php $page_title = 'Staff Menu'; ?>
-<?php include('../../private/shared/staff_header.php'); ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 
 <div id="content">
+  <div is="main-menu">
+    <h2>Main Menu</h2>
+    <ul>
+        <li><a href="<?php echo url_for('/staff/subjects/index.php');
+        ?>">Subjects</a></li>
+    </ul>
+  </div>
 
 </div>
 
 
-<?php include('../../private/shared/staff_footer.php'); ?>
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
